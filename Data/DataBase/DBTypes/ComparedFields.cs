ea467ee6020c34667a62ee9fc6e92dcf983a5490
killbane1232 <killbane1232@yandex.ru>
2024-05-14 14:04:05 +0300

    move to linux

--- Data/DataBase/DBTypes/ComparedFields.cs
+++ Data/DataBase/DBTypes/ComparedFields.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 
 #pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
--- Main/Loggers/LoggerConfigurator.cs
+++ Main/Loggers/LoggerConfigurator.cs
@@ -4,22 +4,37 @@ namespace Arcam.Main.Loggers
 {
     public class LoggerConfigurator
     {
-        public static void Configure(bool flag = false)
-        {
-            if (flag)
-            {
-                LogManager.Setup().LoadConfiguration(builder => {
-                    builder.ForLogger().FilterMinLevel(LogLevel.Warn).WriteToConsole();
-                    builder.ForLogger().FilterMinLevel(LogLevel.Warn).WriteToFile(fileName: "logs/Generations.txt");
-                });
-                return;
-            }
-            LogManager.Setup().LoadConfiguration(builder => {
-                builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToConsole();
-                builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToFile(fileName: "logs/${shortdate}/${logger}.txt");
-                builder.ForLogger().FilterMinLevel(LogLevel.Info).WriteTo(new TelegramLogger());
-            });
+        public enum LogType {
+            main,
+            site,
+            test
         }
+        public static void Configure(LogType type = LogType.main)
+        {
+            switch (type) {
+                case LogType.test:
+                    LogManager.Setup().LoadConfiguration(builder => {
+                        builder.ForLogger().FilterMinLevel(LogLevel.Warn).WriteToConsole();
+                        builder.ForLogger().FilterMinLevel(LogLevel.Warn).WriteToFile(fileName: "logs/Generations.txt");
+                    });
+                    return;
+                case LogType.site:
+                    LogManager.Setup().LoadConfiguration(builder => {
+                        builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToConsole();
+                        builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToFile(fileName: "logs/Generations.txt");
+                    });
+                    return;
+                case LogType.main:
+                    LogManager.Setup().LoadConfiguration(builder => {
+                        builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToConsole();
+                        builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToFile(fileName: "logs/${shortdate}/${logger}.txt");
+                        builder.ForLogger().FilterMinLevel(LogLevel.Info).WriteTo(new TelegramLogger());
+                    
+                    });
+                    return;
+            }
+        }
+
         public static string GetErrorString(Exception ex)
         {
             var str = new StringBuilder();
