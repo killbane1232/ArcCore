15d4df1cbb4b754e1211f5439f72065606d1cf5e
killbane1232 <killbane1232@yandex.ru>
2024-12-16 14:23:54 +0300

    change close position interface

--- Market/IPlatform.cs
+++ Market/IPlatform.cs
@@ -6,7 +6,7 @@ namespace Arcam.Market
     public interface IPlatform
     {
         List<Candle> TakeCandles(WorkingPair pair, int count, Timing timing, bool reverse = false, DateTimeOffset? where = null, bool skipLock = false);
-        string ClosePosition(WorkingPair pair);
+        string ClosePosition(WorkingPair pair, double currentPrice, bool buy);
         string PostOrders(bool buy, double quality, WorkingPair pair);
         Dictionary<string, PositionInfo> EncountPositions();
         WalletInfo TakeWallet(WorkingPair pair);
